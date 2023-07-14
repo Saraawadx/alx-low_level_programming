b16e395333db977c9f4f862fc401a1827dd92e96
sarax <saraawad012@outlook.com>
2023-07-13 20:13:17 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints function
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n);
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
