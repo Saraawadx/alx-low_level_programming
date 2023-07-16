9dc4d3bb0512d43e0f34e0bc10b2a0f5e4a92321
sarax <saraawad012@outlook.com>
2023-07-16 16:09:34 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit)
+
+		if (digit == 57)
+			digigt += 39;
+
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
