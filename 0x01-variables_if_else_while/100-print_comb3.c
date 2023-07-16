626f780223b1ff7e06e8e8fd18a6dafeeb9ff0f6
sarax <saraawad012@outlook.com>
2023-07-16 16:30:06 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
