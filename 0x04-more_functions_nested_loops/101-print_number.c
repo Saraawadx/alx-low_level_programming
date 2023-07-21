381543e94c0a0a4e417c5195e4255f6d7d962b77
sarax <saraawad012@outlook.com>
2023-07-20 23:10:07 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: input
+*/
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_puthar(i % 10 + '0');
+}
