590101c2de75b28449eba51f9d8b0a4afe2a59c0
sarax <saraawad012@outlook.com>
2023-07-20 22:01:55 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,28 +16,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && (i % 5 != 0))
-		{
 			printf("Frizz");
-		}
 		else if (i % 5 == (0 && i % 3 != 0))
-		{
 			printf("Buzz");
-		}
 		else if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FrizzBuzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
-		if (i != 100)
-		{
+		if (i < 100)
 			printf(" ");
-		}
-		else
-			printf("\n");
 	}
-
+	printf("\n");
 	return (0);
 }
