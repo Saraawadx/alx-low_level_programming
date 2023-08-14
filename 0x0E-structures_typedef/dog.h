3addd2d45d15fa3f0dc0012c817d236a6b1d2c3e
sarax <saraawad012@outlook.com>
2023-08-14 14:47:20 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ * @name: dog name
+ * @age: dof age
+ * @owner: owner name
+ * Description: poppy
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
