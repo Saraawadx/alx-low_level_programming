c48b937f4f4b7f5dceae9c65516cdbf4191fcafe
sarax <saraawad012@outlook.com>
2023-08-17 15:12:29 -0700

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct token: struct token
+ * @tok: format token
+ * @fun: function
+*/
+typedef struct tok
+{
+	char *tok;
+	void (*fun)(char *, va_list);
+} tok_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
