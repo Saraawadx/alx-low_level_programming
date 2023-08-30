7cc53f0b53bf966e294768d8a6b8b8f3f65a4cc5
sarax <saraawad012@outlook.com>
2023-08-29 20:05:27 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newNode->n = n;
 	newNode->next = NULL;
-	if (idx == NULL)
+	if (!idx)
 	{
 		newNode->next = *head;
 		*head = newNode;
