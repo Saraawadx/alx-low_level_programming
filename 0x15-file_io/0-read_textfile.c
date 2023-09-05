f7cb226cb011766b4789c051f5cb59a139861b15
sarax <saraawad012@outlook.com>
2023-09-04 17:14:33 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t size;
 	char buffer[READ_BUFFER_SIZE * 8];
 
-	if (filename == NULL || letters == NULL)
+	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
