3a9db804ea399546c33c306005f30a461d22c96c
sarax <saraawad012@outlook.com>
2023-09-04 17:59:09 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,10 +28,8 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t size = 0, length = _strlen(text_content);
 
-	if (!filename)
-		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	if (!filename || fd == -1)
 		return (-1);
 	if (length)
 		size = write(fd, text_content, length);
