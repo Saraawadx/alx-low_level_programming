f30514edb8681609244b29d72d5f4b423a5de823
sarax <saraawad012@outlook.com>
2023-09-04 19:16:46 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,23 +19,23 @@ int main(int ac, char **av)
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 		dprintf(STDERR_FILENO,
-			"Error: Can't read from file NAME_OF_THE_FILE %s\n"
+			"Error: Can't read from file %s\n"
 			, av[1]), exit(98);
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
 			(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 	if (file_to == -1)
 		dprintf(STDERR_FILENO,
-				"Error: Can't write to NAME_OF_THE_FILE %s\n"
+				"Error: Can't write to %s\n"
 				, av[2]), exit(99);
 
 	while ((size = read(file_from, BUFFER, READ_BUFFER_SIZE)) > 0)
 		if (write(file_to, BUFFER, size) != size)
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to NAME_OF_THE_FILE %s\n"
+				"Error: Can't write to %s\n"
 				, av[2]), exit(99);
 	if (size == -1)
 		dprintf(STDERR_FILENO,
-			"Error: Can't read from file NAME_OF_THE_FILE %s\n"
+			"Error: Can't read from file %s\n"
 			, av[1]), exit(98);
 	file_from = close(file_from);
 	file_to = close(file_to);
