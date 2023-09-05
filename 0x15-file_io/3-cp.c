2f6213f18f823fadd3a612bcb6cfa7aa9c66f7bc
sarax <saraawad012@outlook.com>
2023-09-04 19:21:22 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,9 +18,8 @@ int main(int ac, char **av)
 				exit(97);
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n"
-			, av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				av[1]), exit(98);
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
 			(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 	if (file_to == -1)
@@ -40,10 +39,10 @@ int main(int ac, char **av)
 	file_from = close(file_from);
 	file_to = close(file_to);
 	if (file_from)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", file_from),
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from),
 			exit(100);
 	if (file_to)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", file_to),
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to),
 			exit(100);
 	return (1);
 }
