b23ed4ba5b721a9d2d7b20a295b93e0e46522cca
sarax <saraawad012@outlook.com>
2023-09-04 19:11:24 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,27 +9,27 @@
 
 int main(int ac, char **av)
 {
-	int curFD = 0, desFD = 0;
+	int file_from = 0, file_to = 0;
 	ssize_t size;
 	char BUFFER[READ_BUFFER_SIZE];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 				exit(97);
-	curFD = open(av[1], O_RDONLY);
-	if (curFD == -1)
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
 		dprintf(STDERR_FILENO,
 			"Error: Can't read from file NAME_OF_THE_FILE %s\n"
 			, av[1]), exit(98);
-	desFD = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
 			(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
-	if (desFD == -1)
+	if (file_to == -1)
 		dprintf(STDERR_FILENO,
 				"Error: Can't write to NAME_OF_THE_FILE %s\n"
 				, av[2]), exit(99);
 
-	while ((size = read(curFD, BUFFER, READ_BUFFER_SIZE)) > 0)
-		if (write(desFD, BUFFER, size) != size)
+	while ((size = read(file_from, BUFFER, READ_BUFFER_SIZE)) > 0)
+		if (write(file_to, BUFFER, size) != size)
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to NAME_OF_THE_FILE %s\n"
 				, av[2]), exit(99);
@@ -37,13 +37,13 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO,
 			"Error: Can't read from file NAME_OF_THE_FILE %s\n"
 			, av[1]), exit(98);
-	curFD = close(curFD);
-	desFD = close(desFD);
-	if (curFD)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", curFD),
+	file_from = close(file_from);
+	file_to = close(file_to);
+	if (file_from)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", file_from),
 			exit(100);
-	if (desFD)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", curFD),
+	if (file_to)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", file_to),
 			exit(100);
 	return (1);
 }
